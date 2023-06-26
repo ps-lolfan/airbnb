658e9c05ac21ddcd092f7e0fc38da9d390846c72
ps-lolfan <pbsinnur@gmail.com>
2023-06-26 11:16:55 +0530

    initial commit

---
+++ app/components/navbar/Logo.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+const Logo = () => {
+    const router = useRouter()
+    return (
+        <Image
+            src="/images/a2.svg"
+            alt="Logo"
+            className='hidden md:block cursor-pointer'
+            height="100"
+            width="100"
+        />)
+}
+
+export default Logo
