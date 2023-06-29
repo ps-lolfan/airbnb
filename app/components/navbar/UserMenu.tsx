a9d68fa6c116376ebf7eda271ae0762f560d4028
ps-lolfan <pbsinnur@gmail.com>
2023-06-29 11:44:36 +0530

    input box styling

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -3,8 +3,10 @@ import { AiOutlineMenu } from 'react-icons/ai'
 import Avatar from '../Avatar';
 import { useCallback, useState } from 'react';
 import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal';
 
 const UserMenu = () => {
+    const registerModal = useRegisterModal();
     const [isOpen, setIsOpen] = useState(false)
 
     const toggleOpen = useCallback(() => {
@@ -54,7 +56,7 @@ const UserMenu = () => {
                         <>
                             <MenuItem label="Login" onClick={() => { }} />
 
-                            <MenuItem label="Sign up" onClick={() => { }} />
+                            <MenuItem label="Sign up" onClick={registerModal.onOpen} />
                         </>
                     </div>
                 </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
 import ClientOnly from './components/ClientOnly'
-import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
 
 const font = Nunito({ subsets: ['latin'] })
 
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal actionLabel='Submit' isOpen title="hello world" />
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}</body>
