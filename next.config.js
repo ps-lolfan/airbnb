48b026351c718dbad678c7d83439f0e123a75196
ps-lolfan <pbsinnur@gmail.com>
2023-07-01 23:33:12 +0530

    avatar to display image done

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        domains: ['avatars.githubusercontent.com'], // add your own domain here if needed for image loading
+    }
+}
 
 module.exports = nextConfig
