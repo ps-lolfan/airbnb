3f7c846048cf5b65af22f317b7ec9e68381542e6
ps-lolfan <pbsinnur@gmail.com>
2023-07-01 23:20:49 +0530

    github authorization done

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -18,6 +18,7 @@ import Input from '../inputs/Input';
 import { toast } from 'react-hot-toast';
 import Button from '../Button';
 
+import { signIn } from 'next-auth/react'
 
 const RegisterModal = () => {
     const registerModal = useRegisterModal();
@@ -97,7 +98,7 @@ const RegisterModal = () => {
             <Button outline
                 label='Continue with Github'
                 icon={AiFillGithub}
-                onClick={() => { }}
+                onClick={() => signIn('github')}
             />
             <div className='text-neutral-500
             text-center
