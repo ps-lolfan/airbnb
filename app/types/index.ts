c1bcda95add1709b75d072022ca73746016da390
ps-lolfan <pbsinnur@gmail.com>
2023-07-24 17:21:14 +0530

    reservations done with blocking

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,10 +1,20 @@
-import { Listing, User } from "@prisma/client";
+import { Listing, Reservation, User } from "@prisma/client";
 
 export type safeListing = Omit<
 Listing,
 "createdAt"
 > & {
     createdAt: string;
+}
+
+export type SafeReservation = Omit<
+Reservation,
+"createdAt"|"startDate"| "endDate" | "listing"
+> & {
+    createdAt: string;
+    startDate: string;
+    endDate: string;
+    listing: safeListing;
 }
 
 export type SafeUser = Omit<
